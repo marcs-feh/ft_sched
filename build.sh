a5057d7d97c62d2a6182881092e116ea456c9ca8
marcs-feh <marcosfehlauer@protonmail.com>
2025-08-15 22:01:35 -0300

    Update unix build script

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
 
-cc=clang++
+cc='clang++'
 cflags='-std=c++14 -fno-strict-aliasing -fwrapv -O0'
-wflags='-Wall -Wextra -Werror=return-type'
-
-Run(){ echo "$@"; $@; }
+wflags='-Wall -Wextra -Werror=return-type -D_CRT_SECURE_NO_WARNINGS'
 
 set -eu
 
 cflags="$cflags $wflags"
 
-Run $cc $cflags $wflags main.cpp -o ft_sched.exe
+echo [Code generation]
+$cc $cflags $wflags generate.cpp base.cpp -o generate.exe
+./generate.exe
+
+echo [Compile]
+$cc $cflags $wflags main.cpp base.cpp -o ft_sched.exe
+./ft_sched.exe
